25a67f14c268ef292924a172c5cc3e31846973f3
Praveen Yalagandula <ypraveen@gmail.com>
2016-05-02 11:03:54 -0700

    Support for update operation on avi resources

--- avi/heat/avi_resource.py
+++ avi/heat/avi_resource.py
@@ -21,7 +21,6 @@ class AviResource(resource.Resource):
                            self.context.tenant_id)
 
     def get_avi_client(self):
-
         try:
             endpoint = self.client("keystone").url_for(
                 service_type="avi-lbaas",
@@ -30,9 +29,6 @@ class AviResource(resource.Resource):
         except Exception as e:
             LOG.exception("Error during finding avi address: %s", e)
             return None
-        # from IPython.core.debugger import Pdb
-        # pdb = Pdb()
-        # pdb.set_trace()
         username = self.context.auth_token_info['token']['user']['name']
         api_session = ApiSession(
             controller_ip=address,
@@ -69,17 +65,76 @@ class AviResource(resource.Resource):
                           e, self.resource_name, res_def)
             raise
         self.resource_id_set(obj['uuid'])
+        return True
 
-    def _show_resource(self):
-        client = self.get_avi_client()
-        hm = client.get("%s/%s" % (self.resource_name,
+    def _show_resource(self, client=None):
+        if not client:
+            client = self.get_avi_client()
+        obj = client.get("%s/%s" % (self.resource_name,
                                    self.resource_id),
                         tenant_uuid=self.get_avi_tenant_uuid()
                         ).json()
-        return hm
+        return obj
+
+    def _update_obj(self, obj, old_diffs, new_diffs):
+        for p in new_diffs.keys():
+            prev_val = old_diffs[p]
+            new_val = new_diffs[p]
+            if isinstance(new_val, dict) or isinstance(prev_val, dict):
+                if not new_diffs[p]:
+                    obj.pop(p, None)
+                    continue
+                if not obj.get(p, None):
+                    obj[p] = self.create_clean_properties(new_diffs[p])
+                    continue
+                if not prev_val:
+                    old_diffs[p] = prev_val = dict()
+                for k in new_val.keys():
+                    if k not in prev_val:
+                        prev_val[k] = None
+                self._update_obj(obj[p], prev_val, new_val)
+            elif isinstance(new_val, list) or isinstance(prev_val, list):
+                # figure out which entries match from old
+                if not prev_val:
+                    # blindly add new val to obj and return
+                    if not obj.get(p, None):
+                        obj[p] = []
+                    for v in new_val:
+                        obj[p].append(self.create_clean_properties(v))
+                # for each previous entry, find it in obj and replace it
+                # with the entry from the new val
+                # we need key to match
+                # put a debugger and catch it here to understand how to get keys
+                # until then just replace with newval
+                if not new_val:
+                    obj.pop(p, None)
+                else:
+                    obj[p] = self.create_clean_properties(new_val)
+            else:
+                if new_diffs[p]:
+                    obj[p] = new_diffs[p]
+                else:
+                    obj.pop(p, None)
+        return obj
 
     def handle_update(self, json_snippet, tmpl_diff, prop_diff):
-        pass
+        client = self.get_avi_client()
+        obj = self._show_resource(client)
+        # from IPython.core.debugger import Pdb
+        # pdb = Pdb()
+        # pdb.set_trace()
+        self._update_obj(obj, tmpl_diff["Properties"], prop_diff)
+        try:
+            client.put(
+                "%s/%s" % (self.resource_name, self.resource_id),
+                data=obj,
+                tenant_uuid=self.get_avi_tenant_uuid()
+                ).json()
+        except:
+            LOG.exception("Update failed: (%s, %s): %s",
+                          self.resource_name, self.resource_id, obj)
+            raise
+        return True
 
     def handle_delete(self):
         client = self.get_avi_client()
