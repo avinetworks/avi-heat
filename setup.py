ffa79a97344ac4318b50fa278403a7cb0ff58c49
Praveen Yalagandula <ypraveen@avinetworks.com>
2016-06-29 00:53:33 +0000

    Proper versioning: Adding beta notation

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 import os
-import yaml
+import time
+try:
+    import yaml
+except:
+    pass
 from setuptools import setup, find_packages
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-AVI_PIP_VERSION = '16.2'
+ct = time.gmtime()
+date = "%d%02d%02d" % (ct.tm_year, ct.tm_mon, ct.tm_mday)
+AVI_PIP_VERSION = '16.2b' + date
 if os.path.exists("./VERSION"):
     with open("./VERSION", "r") as f:
         fs = yaml.load(f.read())
