3ec5c422d816ba9cac2138182b2aaf03b1eb92ac
Praveen Yalagandula <ypraveen@gmail.com>
2017-06-16 20:12:17 -0700

    AV-22239 bug for update: lists with dicts and get_avi_uuid_by_name not handled well

--- test/unittest/test_utils.py
+++ test/unittest/test_utils.py
@@ -1,5 +1,7 @@
 import unittest
+import copy
 from avi.heat.avi_utils import cmp_a_in_b
+from avi.heat.avi_utils import replace_refs_with_uuids
 
 
 class TestAviResource(unittest.TestCase):
@@ -28,3 +30,24 @@ class TestAviResource(unittest.TestCase):
 
         a["C"] = "C"
         self.assertFalse(cmp_a_in_b(a, b))
+
+    def check_replace_refs_with_uuids(self, obj):
+        print "Before: %s" % obj
+        replace_refs_with_uuids(obj)
+        print "After: %s" % obj
+        self.assertTrue("ref" not in str(obj))
+        self.assertTrue("uuid" in str(obj))
+
+    def test_replace_refs_with_uuids(self):
+        obj = {"single_ref": "http://blah.com/api/vs/vs-21323#vsname",
+               "single_short_ref": "/vs/vs-2322",
+               "multi_refs": ["/vs/vs-23241", "se/se-23412412#dfjha"]
+               }
+        self.check_replace_refs_with_uuids(copy.deepcopy(obj))
+
+        obj1 = {"k1": copy.deepcopy(obj), "k2": None}
+        self.check_replace_refs_with_uuids(obj1)
+
+        obj2 = {"k2": [copy.deepcopy(obj), copy.deepcopy(obj)]}
+        self.check_replace_refs_with_uuids(obj2)
+
